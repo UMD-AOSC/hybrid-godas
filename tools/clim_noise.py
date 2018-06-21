531e2f98e0010826808317f2633676765bedb4ce
Travis Sluka <travis.sluka@noaa.gov>
2018-06-21 14:26:07 -0400

    minor documentation

--- tools/clim_noise.py
+++ tools/clim_noise.py
@@ -5,14 +5,29 @@ import argparse
 from scipy import interpolate
 
 # get command line arguments
-parser=argparse.ArgumentParser(description=( ""))
-parser.add_argument('outfile')
-parser.add_argument('-nx', type=int, required=True)
-parser.add_argument('-ny', type=int, required=True)
-parser.add_argument('-scale', default='100/0.2,30/0.6,3/0.1')
-parser.add_argument('-month', type=int, required=True)
+parser=argparse.ArgumentParser(description=(
+    "Create 2D stochastic noise with a specified correlation length, and values "
+    "ranging from -1 to 1."))
+parser.add_argument('outfile', help=(
+    "filename for the output netCDF file"))
+parser.add_argument('-nx', type=int, required=True, help=(
+    "grid size in the x dimension"))
+parser.add_argument('-ny', type=int, required=True, help=(
+    "grid size in the y dimension"))
+parser.add_argument('-scale', default='100/0.2,30/0.6,3/0.1', help=(
+    "definition of the grid scales for the multiple rounds of perlin noise. "
+    "Format is '<scale>/<strength>,<scale>/<strength>,....'. For example: "
+    "the default of 100/0.2,30/0.6,3/0.1 will create 2D noise with strongest feature "
+    "30 grid points apart, with smaller scale noise (3 gridpoints) and larger scale noise "
+    "(100 grid poitns) added on top as well"))
+parser.add_argument('-month', type=int, required=True, help=(
+    "The current month. This, combined with 'year' and 'seed' ensures "
+    "that the random noise for a given month/year is identical from one "
+    "one to the next. If month == 1 (january) month 12 will be from the previous year."))
 parser.add_argument('-year',  type=int, required=True)
-parser.add_argument('-seed', type=int, required=True)
+parser.add_argument('-seed', type=int, required=True, help=(
+    "random seed for keeping results identical between script runs."))
+
 
 args=parser.parse_args()
 args.scale=[tuple([float(f) for f in a.split("/")]) for a in args.scale.split(",")]
@@ -32,7 +47,6 @@ for mn in range(12):
     if args.month == 1 and mn == 11:
         seed -= 12
 
-#    print(seed)
     np.random.seed(seed)
     for scale in args.scale:
         ngrid=[int(g/scale[0]) for g in grid]
